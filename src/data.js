23a261fd8d45b15dbf71ba5d9814584cb5558670
LokeshMitra <54833464+LokeshMitra@users.noreply.github.com>
2021-07-03 20:04:17 +0530

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,29 @@
+const data = {
+    products: [
+      {
+        id: '1',
+        name: 'Laptop',
+        price: 1400,
+        image: 'https://picsum.photos/id/180/2400/1600',
+      },
+      {
+        id: '2',
+        name: 'Car',
+        price: 2400,
+        image: 'https://i0.wp.com/cutewallpaper.org/21/background-pics-for-car/HD-Car-Background-download-in-zip-file-for-photo-editing-Picsart.jpg',
+      },
+      {
+        id: '3',
+        name: 'Shoes',
+        price: 1000,
+        image: 'https://picsum.photos/id/21/3008/2008',
+      },
+      {
+          id:'4',
+          name: 'Mobile',
+          price: 500,
+          image: 'https://mocah.org/thumbs/524533-apple-digital.jpg'
+      }
+    ],
+  };
+  export default data;
